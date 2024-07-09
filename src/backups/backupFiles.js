0722f22ee688b7e54820806655b8ea7893532aa2
Daniel Hernandez <daniher02@gmail.com>
2024-07-09 21:15:09 +0000

    feat: se agrega el backupfiles

--- src/backups/backupFiles.js
+++ src/backups/backupFiles.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const config = require('../config/config');
 const path = require('path');
 const logger = require('../utils/logger');
@@ -6,18 +6,35 @@ const logger = require('../utils/logger');
 const backupFiles = () => {
   const { sourceDir, backupPath, serverIp, serverUser, sshPort, sshKeyPath } = config;
 
-  const rsyncCommand = `rsync -avz -e "ssh -i ${sshKeyPath} -p ${sshPort}" ${serverUser}@${serverIp}:${sourceDir} ${backupPath}/files`;
+  // Dividir el comando rsync en comando y argumentos
+  const rsyncArgs = [
+    '-avz',
+    '--progress',
+    '--stats',
+    '-e', `ssh -i ${sshKeyPath} -p ${sshPort}`,
+    `${serverUser}@${serverIp}:${sourceDir}`,
+    `${backupPath}/files`
+  ];
 
+  logger.info('Starting file sync...');
+  logger.info(`Executing command: rsync ${rsyncArgs.join(' ')}`);
 
-  exec(rsyncCommand, (error, stdout, stderr) => {
-    if (error) {
-      logger.error(`Error during file sync: ${error.message}`);
-      return;
+  const rsync = spawn('rsync', rsyncArgs);
+
+  rsync.stdout.on('data', (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  rsync.stderr.on('data', (data) => {
+    process.stderr.write(data.toString());
+  });
+
+  rsync.on('close', (code) => {
+    if (code === 0) {
+      logger.info('File sync completed successfully.');
+    } else {
+      logger.error(`File sync process exited with code ${code}`);
     }
-    if (stderr) {
-      logger.warn(`Warnings during file sync: ${stderr}`);
-    }
-    logger.info(`File sync completed successfully: ${stdout}`);
   });
 };
 
